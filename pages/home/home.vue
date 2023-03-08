2bbc5d2fc989b526b3b7f7bb3a0e2812a1c3f982
袁先生 <11535788+qianqian1234567@user.noreply.gitee.com>
2023-03-08 23:26:13 +0800

    tabbar配置完成

---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    <div>123</div>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
